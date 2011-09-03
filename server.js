abc7603af7a80df1f8a4f485fd7552298f232e09
Jack Reilly <jackdreilly@gmail.com>
2011-09-03 09:39:03 -0700

    default statement

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require("http");
 // comment
 // 2nd comment!
+// third comment
 http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
