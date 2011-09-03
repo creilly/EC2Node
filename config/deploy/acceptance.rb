b7f744258c1182e5f9765adbcedd4b1619ddb4a4
Jack Reilly <jackdreilly@gmail.com>
2011-09-03 09:10:09 -0700

    default statement

--- config/deploy/acceptance.rb
+++ config/deploy/acceptance.rb
@@ -1,5 +1,5 @@
 set :node_env, 'acceptance'
-set :branch, 'development'
+set :branch, 'master'
 set :keep_releases, 10
 
 server 'ec2-184-72-12-121.us-west-1.compute.amazonaws.com', :web, :app, :db, :primary => true
